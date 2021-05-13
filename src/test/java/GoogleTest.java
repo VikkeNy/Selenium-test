c6b0a01817460b2d40cea7a488d84b92a1634efc
VikkeNy <modoviktor@hotmail.com>
2021-05-13 11:13:37 +0200

    Changed to the original Selenium test

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -2,9 +2,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,14 +13,13 @@ public class GoogleTest {
 
     @Test
     public void testSearch() throws MalformedURLException {
-    WebDriver driver = new RemoteWebDriver(
-            new URL("http://ec2-3-237-43-149.compute-1.amazonaws.com:4444/wd/hub"), DesiredCapabilities.chrome()
-    );
+    ChromeDriver driver = new ChromeDriver();
+
     driver.manage().window().setSize(new Dimension(1200, 800));
 
         driver.get("https://google.com");
 
-        WebElement searchElement = ((RemoteWebDriver) driver).findElementByName("q");
+        WebElement searchElement = ((ChromeDriver) driver).findElementByName("q");
         searchElement.sendKeys("software testing");
         searchElement.submit();
     }
